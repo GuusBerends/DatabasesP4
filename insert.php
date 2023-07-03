f82a315cb4eacebd061cc01bf7272532b2330d9a
GuusBerends <112177556+GuusBerends@users.noreply.github.com>
2023-07-03 22:27:45 +0200

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,1 +1,13 @@
-insert 
+<?php 
+
+$PDOoptions = [
+    PDO::ATTR_PERSISTENT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_TIMEOUT => 5, // in seconds
+    PDO::MYSQL_ATTR_FOUND_ROWS => true
+];
+
+var_dump($_POST);
+
+$db = null;
+?>
